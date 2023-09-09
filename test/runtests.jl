728472042ec537a8d050ea328dab53851d389f71
Yiyu Ni <niyiyu@uw.edu>
2023-09-09 00:02:10 -0500

    run full test

--- test/runtests.jl
+++ test/runtests.jl
@@ -57,7 +57,7 @@ skip_files = ["test_guess.jl","test_native_io.jl","test_rescale.jl","test_ungap.
               "test_nodal_types.jl","test_processing.jl","test_read_nodal.jl",
               "test_utils.jl", "test_fdsn.jl", "test_iris.jl", "test_seedlink.jl"]
 #for d in ["CoreUtils", "Types", "RandSeis", "Utils", "NativeIO", "DataFormats", "SEED", "Processing", "Nodal", "Quake", "Web"]
-for d in ["Web"] 
+for d in ["CoreUtils", "Types", "RandSeis", "Utils", "NativeIO", "Processing", "Nodal","Web"]
   ld = length(d)
   ll = div(ltn - ld - 2, 2)
   lr = ll + (isodd(ld) ? 1 : 0)
